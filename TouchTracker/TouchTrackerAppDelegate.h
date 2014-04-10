29f7fae6956bf4e786dc7cda2e422931957c88f5
K-RKidd <kr248196@muhlenberg.edu>
2014-04-10 18:54:56 -0400

    Initial Commit

---
+++ TouchTracker/TouchTrackerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TouchTrackerAppDelegate.h
+//  TouchTracker
+//
+//  Created by Krystle on 4/10/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TouchTrackerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Krystle on 4/10/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TouchTrackerAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TouchTrackerAppDelegate class]));
+    }
+}
